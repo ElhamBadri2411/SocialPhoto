fb010134362861596d3f021394045336be65735b
ElhamBadri2411 <elhambadri24@gmail.com>
2021-09-02 12:10:58 -0400

    setup mongoose

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(urlencoded.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
