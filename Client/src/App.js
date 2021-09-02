9ab087e024afe902ca0d8d9693280e2e00e7c607
ElhamBadri2411 <elhambadri24@gmail.com>
2021-09-02 12:08:37 -0400

    setup mongoose

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
+      <h1>App</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
